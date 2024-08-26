a28f06069f5eb85aacf91ca8222316b2a35c806c
Serhii Babskyi <sergbald99@gmail.com>
2024-08-26 23:38:34 +0300

    make build

---
+++ index.0a04dd0e.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table").rows,r=0;r<e.length;r++){var l=e[r].cells;if(l.length>1){var o=l[1].cloneNode(!0),t=l.length-1;e[r].insertBefore(o,l[t])}}
+//# sourceMappingURL=index.0a04dd0e.js.map
